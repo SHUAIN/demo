4cb4be4f976e422873a537c8e4f97246826137bd
shuaxin <986384495@qq.com>
2019-09-28 23:40:54 +0800

    首页完成

---
+++ vue学习/todolist/pages/home.js
@@ -0,0 +1,62 @@
+let html = `
+<div class="home">
+    <ul class="nav">
+        <li>
+            <span class="iconfont icon-instruction"></span>
+        </li>
+        <li class="tittle">TODO</li>
+        <li>
+            <span class="iconfont icon-search"></span>
+        </li>
+    </ul>
+    <ul class="user">
+        <li v-for="(item,index) in user" :key="index">
+            {{item}}
+        </li>
+    </ul>
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide" v-for="(item,index) in cards" :key="index">
+               <p>
+                <span :class=item.icon></span>
+               </p>
+               <p>{{item.name}}</p>
+               <p>{{item.des}}</p>
+            </div>
+        </div>
+    </div>
+</div>
+`;
+
+let home = {
+    template: html,
+    data() {
+        return {
+            user: {
+                name: 'shauxin',
+                about: 'keep passion'
+            },
+            cards: [{
+                name: 'today',
+                icon: 'iconfont icon-edit',
+                des: '今天'
+            },
+            {
+                name: 'plan',
+                icon: 'iconfont icon-clock',
+                des: '计划'
+            },
+            {
+                name: 'all',
+                icon: 'iconfont icon-favoriteslist',
+                des: '全部'
+            }],
+        }
+    },
+    mounted() {
+        var mySwiper = new Swiper('.swiper-container', {
+            // 选项配置
+            // loop: true
+        })
+    }
+}
